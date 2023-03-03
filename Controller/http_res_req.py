fbee46ba9ee3213284cfde1e8ce49d26007467ec
johnnyAraujo11 <johnny17100@gmail.com>
2023-03-03 10:47:45 -0300

    Adiciona metodos getters e setters

--- Controller/http_res_req.py
+++ Controller/http_res_req.py
@@ -1,16 +1,33 @@
 class http_msg():
     
-    def __init__(self, method, status = None,host = None, content = None):
-       self.method
-       self.status
-       self.host
-       self.content
+    def __init__(self, method = None, status = None,host = None, content = None, num_status = None, body = None):
+       self.method = method
+       self.status = status
+       self.host = host
+       self.content = content
+       self.num_status = num_status
+       self.body = body
 
 
-    def response(num_status,status, body):
+    def request():
+        print("request")
+
+    def get_method(self):
+        return self.method
+    
+    def get_status(self):
+        return self.status
+    
+    def get_host(self):
+        return self.host
+    
+    def get_content(self):
+        return self.content
+    
+
+    def response(self):
         print("reponse")
-        return "HTTP/1.0 {} {}\n\n{}".format(num_status, status, body)
-
-
+        return "HTTP/1.0 {} {}\n\n{}".format(self.num_status, self.status, self.body)
+    
     def request():
         print("request")
