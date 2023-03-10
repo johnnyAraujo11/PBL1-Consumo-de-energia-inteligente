23b9204529169fcf5ae052f9ca8d66e9489cd6fc
johnnyAraujo11 <johnny17100@gmail.com>
2023-03-10 12:01:37 -0300

    desenvolvimento

--- Controller/device.py
+++ Controller/device.py
@@ -5,7 +5,7 @@ import threading
 class Device():
     
     def __init__(self):
-        self.watt = 0 
+        self.watt = 1000 
         self.time = 0
 
     def consuming(self):
@@ -14,8 +14,8 @@ class Device():
             sleep(1)
             self.time += 1
 
-    def get_consumption(self):
-        return (self.watt * (self.time/60))
+    def get_watt(self):
+        return self.watt
     
 
     def set_watt(self, new_watt):
--- Controller/measurerUDP.py
+++ Controller/measurerUDP.py
@@ -2,32 +2,41 @@ from time import sleep
 import threading
 import socket
 import device
+import json
+
 
 class MeasurerUDP():
 
-    def __init__(self, host='localhost', port=5000) -> None:
+    def __init__(self, device, host='localhost', port=5000) -> None:
         self.port = port
         self.host = host
         self.UDP_client = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.buffer_size = 1024
+        self.device = device
 
-    def send_message(self, device):
-        msg_bytes = str.encode('{"id":"001","pontecy":{}, "date": "08/03/2023", "time_on":"500", "hour":"10:00"}'.format(device.get_consumption()))
+    def send_message(self):
+        #msg = '{"id":"001", "pontecy": 100, "date": "08/03/2023", "time_on":"500", "hour":"10:00"}' 
+        
+        msg = {"id":"001", "consumption": device.get_watt(), "date": "08/03/2023", "time_on":"500", "hour":"10:00"}
+        json_msg = json.dumps(msg)
+        
+        msg_bytes = str.encode(json_msg)
         self.UDP_client.sendto(msg_bytes, (self.host, self.port))
 
 
-    def time_to_send_message(self, device):
+    def time_to_send_message(self):
         while(True):
             sleep(5)
-            self.send_message(device)
+            self.send_message()
 
 
-    def start(self, device):
-        threading.Thread(target=self.time_to_send_message,args=(device,), daemon=True).start()
+    def start(self):
+        threading.Thread(target=self.time_to_send_message, daemon=True).start()
         t = 0
         #somente para executar a thread em segundo plano
         while(True):
-            watt = int(input("O consumo atual é x digite a potência para alterar o consumo: "))
+            device.watt = int(input("O consumo atual é x digite a potência para alterar o consumo: "))
+            
             sleep(1)
             t += 1
             print(t)
@@ -35,7 +44,7 @@ class MeasurerUDP():
 device = device.Device()
 device.start()
 
-measurer = MeasurerUDP()
-measurer.start(device)
+measurer = MeasurerUDP(device)
+measurer.start()
 
  
